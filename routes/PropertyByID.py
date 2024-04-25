8a4550217c5aa4b85ceb2117a56d74ffaf7eaf4c
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-04-25 10:51:48 +0300

    added delete for property by Id

--- routes/PropertyByID.py
+++ routes/PropertyByID.py
@@ -8,7 +8,7 @@ class PropertyByID(Resource):
         property = Property.query.filter_by(id=id).first()
             
         if not property:
-            return {"error":"Property does not exist"}
+            return {"error":"Property does not exist"}, 404
 
         response = make_response(
             jsonify(property.to_dict(view_owner=True)),
@@ -19,5 +19,15 @@ class PropertyByID(Resource):
     
     
     def delete(self, id):
-        return "Route for deleting Property By ID"
+        property = Property.query.filter_by(id=id).first()
+            
+        if not property:
+            return {"error":"Property does not exist"}, 404
     
+        try:
+            db.session.delete(property)
+            db.session.commit()
+            return {"success": "deleted succesfully"}, 204
+        except Exception as e:
+            db.session.rollback()
+            return {"error": str(e)}, 500
