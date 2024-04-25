bcb88f7063bbe7f3a46a7f54b8fb5e69bdae68f2
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-04-25 10:12:58 +0300

    added get and delete for user by id

--- routes/UserByID.py
+++ routes/UserByID.py
@@ -1,11 +1,33 @@
+from flask import jsonify, make_response
 from flask_restful import Resource
+from lib import db, User
 
 class UserByID(Resource):
     
     def get(self, id):
-        return "Users By ID will be here"
+        user = User.query.filter_by(id=id).first()
+            
+        if not user:
+            return {"error":"User does not exist"}
+
+        response = make_response(
+            jsonify(user.to_dict()),
+            200
+        )
+        
+        return response
     
     
     def delete(self, id):
-        return "Route for deleting User By ID"
-    
+        user = User.query.filter_by(id=id).first()
+            
+        if not user:
+            return {"error":"User does not exist"}
+
+        try:
+            db.session.delete(user)
+            db.session.commit()
+            return {}, 204
+        except Exception as e:
+            db.session.rollback()
+            return {"error": str(e)}, 500
